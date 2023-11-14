3b55aa13a4b4b791b88ecd06177b02e2fe0884fd
r.jaepel <r.jaepel@fz-juelich.de>
2023-11-14 14:19:16 +0100

    Fix ssh_to_http conversion

--- cadetrdm/utils.py
+++ cadetrdm/utils.py
@@ -1,3 +1,3 @@
 def ssh_url_to_http_url(url):
-    url.replace(":", "/").replace("git@", "https://").replace(".git", "")
+    url = url.replace(":", "/").replace("git@", "https://").replace(".git", "")
     return url
