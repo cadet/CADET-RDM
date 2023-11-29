a2beb9fcd574b8aad723a9db8a714a3ae75751b3
r.jaepel <r.jaepel@fz-juelich.de>
2023-11-29 14:42:21 +0100

    bump version to 0.0.8

--- cadetrdm/__init__.py
+++ cadetrdm/__init__.py
@@ -2,4 +2,4 @@ from .repositories import ProjectRepo
 from .initialize_repo import initialize_repo, clone
 from .conda_env_utils import prepare_conda_env
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
