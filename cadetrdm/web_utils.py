beadae4960ca334d341fb9901e0aaeb9aad1421f
r.jaepel <ronald.jaepel@gmail.com>
2024-03-06 09:50:07 +0100

    Allow cloning from local paths

--- cadetrdm/web_utils.py
+++ cadetrdm/web_utils.py
@@ -1,8 +1,11 @@
 from urllib.parse import urlparse
+from pathlib import Path
 
 
 def ssh_url_to_http_url(url):
     if "https" in url:
+        return url
+    if Path(url).exists():
         return url
 
     url = url.replace(":", "/").replace("git@", "https://").replace(".git", "")
