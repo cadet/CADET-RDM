b26919b401e78a15deed573ed843c66043fd8071
r.jaepel <ronald.jaepel@gmail.com>
2025-01-28 16:12:39 +0100

    Add environment class

--- cadetrdm/logging.py
+++ cadetrdm/logging.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-import re
+from typing import List
 
-import semantic_version
 from tabulate import tabulate
-import yaml
+
+from cadetrdm.environment import Environment
 
 class OutputLog:
     def __init__(self, filepath=None):
@@ -15,7 +15,7 @@ class OutputLog:
 
         self._filepath = filepath
         self._entry_list = self._read_file(filepath)
-        self.entries = self._entries_from_entry_list(self._entry_list)
+        self.entries: List[LogEntry] = self._entries_from_entry_list(self._entry_list)
 
     def __iter__(self):
         self._index = 0
@@ -71,34 +71,21 @@ class LogEntry:
         self.tags = tags
         self.options_hash = options_hash
         self._filepath = filepath
-        self._packages = None
+        self._environment: Environment = None
         for key, value in kwargs:
             setattr(self, key, value)
 
     def __repr__(self):
         return f"OutputEntry('{self.output_repo_commit_message}', '{self.output_repo_branch}')"
 
-    def _load_packages(self):
+    def _load_environment(self):
         environment_path = (
                 Path(self._filepath).parent
                 / "run_history"
                 / self.output_repo_branch
                 / "conda_environment.yml"
         )
-        with open(environment_path) as handle:
-            yml_string = "".join(handle.readlines())
-            ansi_escape_pattern = re.compile(r'\x1b\[[0-9;]*[a-zA-Z]')
-            yml_string = re.sub(ansi_escape_pattern, "", yml_string)
-            packages = yaml.safe_load(yml_string)
-
-        conda_packages = packages["dependencies"]
-        conda_packages = {line.split("=")[0]: line.split("=")[1] for line in conda_packages if isinstance(line, str)}
-        self._packages = conda_packages
-
-        if "pip" in packages["dependencies"][-1].keys():
-            pip_packages = packages["dependencies"][-1]["pip"]
-            pip_packages = {line.split("==")[0]: line.split("==")[1] for line in pip_packages}
-            self._packages.update(pip_packages)
+        self._environment = Environment.from_yml(environment_path)
 
     def package_version(self, package):
         """
@@ -110,12 +97,12 @@ class LogEntry:
         Returns:
             str: The version of the specified package.
         """
-        if self._packages is None:
-            self._load_packages()
+        if self._environment is None:
+            self._load_environment()
 
-        return self._packages[package]
+        return self._environment[package]
 
-    def check_package_version(self, package: str, version: str):
+    def fulfils(self, package: str, version: str):
         """
         Checks if the installed version of a package matches the specified version.
 
@@ -133,21 +120,7 @@ class LogEntry:
 
         Uses semantic versioning to compare the versions.
         """
-        if self._packages is None:
-            self._load_packages()
+        if self._environment is None:
+            self._load_environment()
 
-        package_version = self.package_version(package)
-
-        return semantic_version.match(version, package_version)
-
-
-if __name__ == '__main__':
-    output_log = OutputLog(filepath=r"C:\Users\ronal\PycharmProjects\CADET-RDM\tests\test_repo\results\log.tsv")
-    print(output_log.entries)
-    print(output_log.entries[1].package_version("cadet"))
-    print(output_log.entries[1].check_package_version("cadet", ">4.4.0"))
-    print(output_log.entries[1].check_package_version("cadet", "~4.4.0"))
-
-    output_log = OutputLog(
-        filepath=r"C:\Users\ronal\PycharmProjects\CADET-RDM\tests\test_repo\results\lognonexistant.tsv")
-    print(output_log.entries)
+        return self._environment.fulfils(package, version)
