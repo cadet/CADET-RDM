7bf5200b8d845632a57fc757dbb4ca6159e7db87
Katharina Paul <katharina.paul2403@gmail.com>
2025-07-23 14:00:09 +0200

    Expand output README to match template

--- cadetrdm/initialize_repo.py
+++ cadetrdm/initialize_repo.py
@@ -241,7 +241,12 @@ def create_readme():
 
 
 def create_output_readme():
-    readme_lines = ["## Project Repository",
+    readme_lines = ["# Output repository for Example Simulation with CADET",
+                    "This repository stores the simulation results for RDM-Example. `CADET-RDM` automatically tracks all simulations that are started by running `main.py` from the corresponding project repository.",
+                    "",
+                    "Each simulation run creates a dedicated branch in this output repository. The results are saved within the `src` folder of the respective branch. Additionally, a `log.tsv` file in the main branch records metadata for all runs, uniquely linking each output branch to its originating run in the project repository.",
+                    "",
+                    "## Project Repository",
                     "",
                     "The project repository for this case study is available here:",
                     "[Link to Project Repository]() (not actually set yet because no remote has been configured at this moment)"]
